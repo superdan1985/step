95885513447977d5b5588aa545c494ee39d80b8b
superdan1985 <141065508+superdan1985@users.noreply.github.com>
2023-09-15 10:56:50 +0300

    Add files via upload
    some js files

--- app.js
+++ app.js
@@ -1,62 +1,12 @@
-const slider = document.querySelector("#slider");
+const nav = document.getElementById("nav")
+const burger = document.getElementById("burger")
 
-const slides = [
-  {
-    id: 1,
-    title: "My slide 1",
-    description: "Slide description 1",
-    image:
-      "https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-  },
-  {
-    id: 2,
-    title: "My slide 2",
-    description: "Slide description 2",
-    image:
-      "https://images.unsplash.com/photo-1549317661-bd32c8ce0db2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-  },
-  {
-    id: 3,
-    title: "My slide 3",
-    description: "Slide description 3",
-    image:
-      "https://images.unsplash.com/photo-1590362891991-f776e747a588?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80",
-  },
-  {
-    id: 4,
-    title: "My slide 4",
-    description: "Slide description 4",
-    image:
-      "https://images.unsplash.com/photo-1502489597346-dad15683d4c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2148&q=80",
-  },
-  {
-    id: 5,
-    title: "My slide 5",
-    description: "Slide description 5",
-    image:
-      "https://images.unsplash.com/photo-1610899600551-a30bf595959e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
-  },
-  {
-    id: 6,
-    title: "My slide 6",
-    description: "Slide description 6",
-    image:
-      "https://images.unsplash.com/photo-1567807708938-79bb7cda5e23?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=985&q=80",
-  },
-];
-
-if (slides.length % 2 != 0) {
-  alert("Number of slides must be even");
-}
-
-slider.style.width = `${slides.length * 350}px`;
-
-slides.forEach((slide) => {
-  slider.innerHTML += `
-        <div class='slide'>
-            <h1>${slide.title}</h1>
-            <p>${slide.description}</p>
-            <img src="${slide.image}">
-        </div>
-    `;
-});
+burger.addEventListener("click", () => {
+    if (nav.classList.contains("show-nav")) {
+        nav.classList.remove("show-nav")
+        burger.classList.remove("open")
+    } else {
+        nav.classList.add("show-nav")
+        burger.classList.add("open")
+    }
+})
